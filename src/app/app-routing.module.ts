057be89024d9ed2e3938de37479bca607de65aac
ah.almeshaal <ah.alameshaal@spl.com.sa>
2022-11-20 16:01:10 +0300

    Generate Components and Set the Routing For The Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from "./products/products.component";
+import { AboutUsComponent } from "./about-us/about-us.component";
+import { ContactUsComponent } from "./contact-us/contact-us.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products-component', component: ProductsComponent },
+  { path: 'about-us-component', component: AboutUsComponent },
+  { path: 'contact-us-component', component: ContactUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
