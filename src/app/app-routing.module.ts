a942c3dfffb43d572f307281e01495ba65076ef3
ah.almeshaal <ah.alameshaal@spl.com.sa>
2022-11-21 09:53:14 +0300

    Create Nav-Header and Footer Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from "./products/products.component";
 import { AboutUsComponent } from "./about-us/about-us.component";
 import { ContactUsComponent } from "./contact-us/contact-us.component";
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-  { path: 'products-component', component: ProductsComponent },
-  { path: 'about-us-component', component: AboutUsComponent },
-  { path: 'contact-us-component', component: ContactUsComponent },
+  { path: 'products', component: ProductsComponent, title: 'PRODUCTS'},
+  { path: 'about-us', component: AboutUsComponent, title: 'ABOUT US'},
+  { path: 'contact-us', component: ContactUsComponent, title: 'CONTACT US'},
+  { path: 'home', component: HomeComponent, title: 'HOME' },
+  { path: '', redirectTo: 'home-component', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { NavHeaderComponent } from './layout/nav-header/nav-header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
     AboutUsComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    HomeComponent,
+    NavHeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
